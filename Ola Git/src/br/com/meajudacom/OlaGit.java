aa1acbb181df4ee9e795cfe3f79be726cdceb646
gusta <gusta@DESKTOP-Q3VPK1O>
2022-09-17 15:04:02 -0300

    adicionei uma nova linha ao codigo

--- Ola Git/src/br/com/meajudacom/OlaGit.java
+++ Ola Git/src/br/com/meajudacom/OlaGit.java
@@ -3,6 +3,7 @@ package br.com.meajudacom;
 public class OlaGit {
 	public static void main(String[] args) {
 		System.out.println("ola Git, ve se da certo...");
+		System.out.println("ve se o commit foi");
 	}
 
 }
