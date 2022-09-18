ee68da4cec5a069bedec7f42018885f9ba414513
gusta <gusta@DESKTOP-Q3VPK1O>
2022-09-18 13:53:08 -0300

    vamos ve se vai usando o token e meu nome no perfil no lugar de minha senha
    Change-Id: I9f05ac1651d88084df6d755751be450849013af4

--- Ola Git/src/br/com/meajudacom/OlaGit.java
+++ Ola Git/src/br/com/meajudacom/OlaGit.java
@@ -4,6 +4,11 @@ public class OlaGit {
 	public static void main(String[] args) {
 		System.out.println("ola Git, ve se da certo...");
 		System.out.println("ve se o commit foi");
+		System.out.println("e agora?");
+		System.out.println("e agora?");
+		System.out.println("e agora?");
+		System.out.println("e nada ainda?");
+		System.out.println("ainda nao tinha instalado o git... sera que  agora vai?");
 	}
 
 }
